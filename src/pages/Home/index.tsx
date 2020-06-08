40addd1ac87358df2c0974c33d3275a69d71f622
Sara Gomes <sarah.gomes80@hotmail.com>
2020-06-07 21:12:36 -0300

    Construção do HTML da primeira página

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
+
+import './style.css'
+
+import logo from '../../assets/logo.svg'
+
+const Home = () => {
+    return (
+       <div id="page-home">
+           <div className="content">
+               <header>
+                <img src={logo} alt="Ecoleta" />
+               </header>
+                
+                <main>
+                    <h1>Seu marketplace de coleta de resíduos.</h1>
+                    <p>Ajudamos pessoas a encontrar pontos de coleta de forma eficiente.</p>
+
+                    <a href="/cadastro">
+                        <span>
+                            <FiLogIn />
+                        </span>
+                        <strong>Cadastre um ponto de coelta</strong>
+                    </a>
+                </main>
+           </div>
+       </div>
+    )
+}
+
+export default Home
